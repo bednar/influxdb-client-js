5a49a12ddb557eae4d1a3ccf9e42a4f67d7b0b88
Jakub Bednar <jakub.bednar@gmail.com>
2020-04-15 09:35:24 +0200

    chore: move from to Makefile to separate script (sed, awk scripts are not multiplatform)

--- .circleci/deploy-nightly-version.sh
+++ .circleci/deploy-nightly-version.sh
@@ -1,5 +1,7 @@
 set -e
 
+SCRIPT_PATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
+
 VERSION=$(cat packages/core/src/impl/version.ts | sed 's/[^0-9.]*//g' | awk -F. '{$2+=1; OFS="."; print $1"."$2"."$3}')
 sed -i -e "s/CLIENT_LIB_VERSION = '.*'/CLIENT_LIB_VERSION = '${VERSION}.nightly'/" packages/core/src/impl/version.ts
 cat packages/core/src/impl/version.ts
@@ -8,5 +10,12 @@ cat packages/core/src/impl/version.ts
 git config user.name "CircleCI Builder"
 git config user.email "noreply@influxdata.com"
 git commit -am "chore(release): prepare to release influxdb-client-js-${VERSION}.nightly"
-yarn run build
+
+# Build
+cd "${SCRIPT_PATH}"/../packages/core
+yarn build
+cd "${SCRIPT_PATH}"/../packages/apis
+yarn build
+
+# Publish
 yarn lerna publish --canary preminor --no-git-tag-version --force-publish --preid nightly --yes || true
