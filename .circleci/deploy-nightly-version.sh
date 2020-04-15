3d1664ca8aa1ce9efe90d814d5a2fcb8176fdbaf
Jakub Bednar <jakub.bednar@gmail.com>
2020-04-15 10:47:51 +0200

    chore: move from to Makefile to separate script (sed, awk scripts are not multiplatform)

--- .circleci/deploy-nightly-version.sh
+++ .circleci/deploy-nightly-version.sh
@@ -2,23 +2,23 @@ set -e
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
 
+# Update Version
 VERSION=$(cat packages/core/src/impl/version.ts | sed 's/[^0-9.]*//g' | awk -F. '{$2+=1; OFS="."; print $1"."$2"."$3}')
 sed -i -e "s/CLIENT_LIB_VERSION = '.*'/CLIENT_LIB_VERSION = '${VERSION}.nightly'/" packages/core/src/impl/version.ts
-cat packages/core/src/impl/version.ts
-cat packages/core/src/impl/version.ts
-cat packages/core/src/impl/version.ts
 git config user.name "CircleCI Builder"
 git config user.email "noreply@influxdata.com"
 git commit -am "chore(release): prepare to release influxdb-client-js-${VERSION}.nightly"
 
-# Build
+# Build Core
 cd "${SCRIPT_PATH}"/../packages/core
-npm install
+npm ci
 yarn add -D @typescript-eslint/eslint-plugin
 yarn build
+
+# Build Apis
 cd "${SCRIPT_PATH}"/../packages/apis
-npm install
+npm ci
 yarn build
 
 # Publish
-yarn lerna publish --canary preminor --no-git-tag-version --force-publish --preid nightly --yes || true
+yarn lerna publish --canary preminor --no-git-tag-version --force-publish --preid nightly --yes
